0e3616d295da8a68996bbb338b707359152ea103
Kuba <kubawernerowski@gmail.com>
2018-11-20 15:24:58 -0500

    Added view-model-accessible methods to User Repo.

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/gamecentre/Database/UserRepository.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/gamecentre/Database/UserRepository.java
@@ -39,9 +39,34 @@ public class UserRepository implements AsyncUserResult {
         userDao = db.userDao();
     }
 
+    /**
+     * Insert a user into the database.
+     *
+     * @param user the user
+     */
     public void insertUser(User user) {
-//        new queryInsertAsyncTask.
-    }
+        new queryAsyncTask.insertAsyncTask(userDao).execute(user);
+   }
+
+    /**
+     * Stores a specific user into currentUser by username.
+     *
+     * @param username  the username of the user.
+     */
+   public void getUserByUsername(String username) {
+        queryAsyncTask.getUserAsyncTask task = new queryAsyncTask.getUserAsyncTask(userDao);
+        task.delegate = this;
+        task.execute(username);
+   }
+
+   /**
+    * Stores all users in the database into allUsers.
+    */
+   public void getAllUsers() {
+        queryAsyncTask task = new queryAsyncTask(userDao);
+        task.delegate = this;
+        task.execute();
+   }
 
     @Override
     public void asyncFinished(User user) {
@@ -49,9 +74,11 @@ public class UserRepository implements AsyncUserResult {
     }
 
     /**
-     * The async task for getting all users.
+     * The async task for getting all user operations.
+     * queryAsyncTask's outside layer handles getting all users in the db.
+     * The private inner classes are for retrieving indiviudal users and inserting users.
      */
-    private static class queryAsyncGetAllTask extends AsyncTask<Void, Void, List<User>> {
+    private static class queryAsyncTask extends AsyncTask<Void, Void, List<User>> {
         /**
          * The User DAO.
          */
@@ -67,12 +94,12 @@ public class UserRepository implements AsyncUserResult {
          *
          * @param dao the dao
          */
-        queryAsyncGetAllTask(UserDao dao) {
+        queryAsyncTask(UserDao dao) {
             asyncTaskDao = dao;
         }
 
         @Override
-        protected List<User> doInBackground(Void... voids) {
+        protected List<User> doInBackground(final Void... voids) {
             return asyncTaskDao.getAll();
         }
 
@@ -80,65 +107,70 @@ public class UserRepository implements AsyncUserResult {
         protected void onPostExecute(List<User> users) {
             delegate.allUsers.setValue(users);
         }
+
+        /**
+         * The async task for inserting users.
+         */
+        private static class insertAsyncTask extends AsyncTask<User, Void, Void> {
+            /**
+             * The User DAO.
+             */
+            private UserDao asyncTaskDao;
+
+            /**
+             * Initializes the insert async task.
+             *
+             * @param dao   the user dao.
+             */
+            insertAsyncTask(UserDao dao) {
+                asyncTaskDao = dao;
+            }
+
+            @Override
+            protected Void doInBackground(final User... params) {
+                asyncTaskDao.insert(params[0]);
+                return null;
+            }
+        }
+
+        /**
+         * The Async task for retrieving a user.
+         */
+        private static class getUserAsyncTask extends AsyncTask<String, Void, User> {
+            /**
+             * The User DAO.
+             */
+            private UserDao asyncTaskDao;
+            /**
+             * The User Repository instance.
+             */
+            private UserRepository delegate = null;
+
+            /**
+             * Instantiates a new async task for users..
+             *
+             * @param dao the dao
+             */
+            getUserAsyncTask(UserDao dao) {
+                asyncTaskDao = dao;
+            }
+
+            @Override
+            protected User doInBackground(final String... params) {
+                return asyncTaskDao.findByName(params[0]);
+            }
+
+            @Override
+            protected void onPostExecute(User result) {
+                delegate.asyncFinished(result);
+            }
+        }
+
     }
 
-    /**
-     * The Async task for retrieving a user.
-     */
-    private static class queryAsyncGetUserTask extends AsyncTask<String, Void, User> {
-        /**
-         * The User DAO.
-         */
-        private UserDao asyncTaskDao;
-        /**
-         * The User Repository instance.
-         */
-        private UserRepository delegate = null;
 
-        /**
-         * Instantiates a new async task for users..
-         *
-         * @param dao the dao
-         */
-        queryAsyncGetUserTask(UserDao dao) {
-            asyncTaskDao = dao;
-        }
 
-        @Override
-        protected User doInBackground(final String... params) {
-            return asyncTaskDao.findByName(params[0]);
-        }
 
-        @Override
-        protected void onPostExecute(User result) {
-            delegate.asyncFinished(result);
-        }
-    }
-
-    /**
-     * The async task for inserting users.
-     */
-    private static class queryInsertAsyncTask extends AsyncTask<User, Void, Void> {
-        /**
-         * The User DAO.
-         */
-        private UserDao asyncTaskDao;
-
-        /**
-         * Initializes the insert async task.
-         *
-         * @param dao   the user dao.
-         */
-        queryInsertAsyncTask(UserDao dao) {
-            asyncTaskDao = dao;
-        }
-
-        @Override
-        protected Void doInBackground(final User... params) {
-            asyncTaskDao.insert(params[0]);
-            return null;
-        }
-    }
 
 
 }
