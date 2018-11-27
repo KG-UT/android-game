4a0ceef7923b8995ff94543fc4b407bf5a650153
rohit <rohit.bansal@mail.utoronto.ca>
2018-11-27 02:48:17 -0500

    In class SlidingTileBoardManager: changed access of canUndo() and undoMove() to public in order to test

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/gamecentre/slidingTile/SlidingTileBoardManager.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/gamecentre/slidingTile/SlidingTileBoardManager.java
@@ -205,7 +205,7 @@ public class SlidingTileBoardManager extends BoardManager {
      *
      * @return a boolean showing if there are moves in the stack of moves.
      */
-    boolean canUndo(){
+   public boolean canUndo(){
         return !stackOfMoves.isEmpty();
     }
 
@@ -214,7 +214,8 @@ public class SlidingTileBoardManager extends BoardManager {
      *
      * PRECONDITION: THE MOVE STACK IS NOT EMPTY
      */
-    void undoMove() {
+
+    public void undoMove() {
         int[] backPosition = stackOfMoves.pop();
         int row = backPosition[0];
         int col = backPosition[1];
