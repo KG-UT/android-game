7d11d24f4056896e7c03ea9c39cbdf1f56e09c7c
KG-UT <33611999+KG-UT@users.noreply.github.com>
2018-11-27 21:54:56 -0500

    Implemented the Tile class for Matching Cards.

---
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/gamecentre/games/matchingCards/MatchingCardsTile.java
@@ -0,0 +1,50 @@
+package fall2018.csc2017.gamecentre.games.matchingCards;
+
+import java.io.Serializable;
+
+public class MatchingCardsTile implements Serializable {
+
+    /**
+     * A boolean that states if the current tile is face up.
+     */
+    private boolean faceUp = false;
+
+    /**
+     * The number of the card.
+     */
+    private int cardNumber;
+
+    /**
+     * A tile with the cardNumber given by number.
+     *
+     * @param number the card number.
+     */
+    MatchingCardsTile(int number){
+        this.cardNumber = number;
+    }
+
+    /**
+     * Sets the card face up.
+     */
+    void setFaceUp(){faceUp = true;}
+
+    /**
+     * Sets the card face down.
+     */
+    void setFaceDown(){faceUp = false;}
+
+    /**
+     * Gets the card number.
+     * @return the card number.
+     */
+    int getNumber(){return cardNumber;}
+
+    /**
+     * Returns if this card is the same as another card, for the purpose of comparison.
+     * @param other Another MatchingCardsTile.
+     * @return a boolean stating if this card is equal to another.
+     */
+    boolean isequal(MatchingCardsTile other){
+        return this.getNumber() == other.getNumber();
+    }
+}
