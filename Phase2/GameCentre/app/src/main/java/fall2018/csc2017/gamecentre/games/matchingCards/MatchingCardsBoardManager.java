f4c57af59917cdec103b80bbfd07f785e5e80767
KG-UT <33611999+KG-UT@users.noreply.github.com>
2018-11-28 02:14:19 -0500

    Implemented some constructors for BoardManager.

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/gamecentre/games/matchingCards/MatchingCardsBoardManager.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/gamecentre/games/matchingCards/MatchingCardsBoardManager.java
@@ -1,6 +1,45 @@
 package fall2018.csc2017.gamecentre.games.matchingCards;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import fall2018.csc2017.gamecentre.game.BoardManager;
 
+/**
+ * Manage the Board, flipping cards up/down, checking for a win
+ */
 public class MatchingCardsBoardManager extends BoardManager {
+    private int score = 0;
+
+    /**
+     * Manage a new 4 by 4 shuffled board
+     */
+    MatchingCardsBoardManager() {
+        this(4, 4);
+    }
+
+    /**
+     * Manage a board that has been pre-populated.
+     * @param board the board
+     */
+    public MatchingCardsBoardManager(MatchingCardsBoard board) {
+        super(board);
+    }
+
+    /**
+     * Manage a new shuffled board.
+     * PRECONDITION: numRows*numCols must be an even number
+     */
+    public MatchingCardsBoardManager(int numRows, int numCols) {
+        List<MatchingCardsTile> tiles = new ArrayList<>();
+        final int numTiles = numRows * numCols;
+        for (int tileNum = 0; tileNum != numTiles/2; tileNum++) {
+            tiles.add(new MatchingCardsTile(tileNum));
+            tiles.add(new MatchingCardsTile(tileNum));
+        }
+
+        tiles.shuffle();
+        setBoard(new SlidingTileBoard(numRows, numCols, tiles));
+        shuffle();
+    }
 }
