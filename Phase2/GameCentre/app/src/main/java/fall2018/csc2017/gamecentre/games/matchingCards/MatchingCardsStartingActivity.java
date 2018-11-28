7096a2ba02199387acbbd2e751b681655adde36f
rohit <rohit.bansal@mail.utoronto.ca>
2018-11-28 18:28:39 -0500

    In MatchingCardsStartingActivity : defined autosave .ser file

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/gamecentre/games/matchingCards/MatchingCardsStartingActivity.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/gamecentre/games/matchingCards/MatchingCardsStartingActivity.java
@@ -5,4 +5,5 @@ import fall2018.csc2017.gamecentre.GameStartingActivity;
 public class MatchingCardsStartingActivity extends GameStartingActivity {
 
     public void saveToFile(String fileName){};
+    public static final String SAVE_FILENAME = "Matching_Cards_autosave_file.ser";
 }
