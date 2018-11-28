451abd1c696ddb43bda6ac0323358eeb68c7ff11
KG-UT <33611999+KG-UT@users.noreply.github.com>
2018-11-27 22:08:55 -0500

    Implemented constructor for MatchingCardsBoard.

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/gamecentre/games/matchingCards/MatchingCardsBoard.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/gamecentre/games/matchingCards/MatchingCardsBoard.java
@@ -1,6 +1,21 @@
 package fall2018.csc2017.gamecentre.games.matchingCards;
 
 import fall2018.csc2017.gamecentre.game.Board;
+import java.util.List;
 
 public class MatchingCardsBoard extends Board {
+
+    /**
+     * A new board of tiles in row-major order.
+     * Precondition: len(tiles) == NUM_ROWS * NUM_COLS
+     *
+     * @param rows the number of rows
+     * @param cols the number of columns
+     * @param tiles the tiles for the board
+     */
+    public MatchingCardsBoard(int rows, int cols, List<MatchingCardsTile> tiles){
+        super(rows, cols, tiles);
+    }
+
+
 }
