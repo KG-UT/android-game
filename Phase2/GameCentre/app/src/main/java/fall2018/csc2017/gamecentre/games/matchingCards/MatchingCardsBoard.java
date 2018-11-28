70b600baad9774670536b65209299781c49b5483
KG-UT <33611999+KG-UT@users.noreply.github.com>
2018-11-28 01:45:34 -0500

    Implemented MatchingCardsBoard.

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/gamecentre/games/matchingCards/MatchingCardsBoard.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/gamecentre/games/matchingCards/MatchingCardsBoard.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class MatchingCardsBoard extends Board {
 
+    private boolean cardIsUp = false;
     /**
      * A new board of tiles in row-major order.
      * Precondition: len(tiles) == NUM_ROWS * NUM_COLS
@@ -17,5 +18,36 @@ public class MatchingCardsBoard extends Board {
         super(rows, cols, tiles);
     }
 
+    /**
+     * return the MatchingCardsTile at (row, col)
+     * @param row the MatchingCardsTile row
+     * @param col the MatchingCardsTile column
+     * @return The matchingCardsTile
+     */
+    MatchingCardsTile getCard(int row, int col){return (MatchingCardsTile) getItem(row, col);}
 
+    /**
+     * flips the card at (row, col) up
+     * @param row the MatchingCardsTile row
+     * @param column the MatchingCardsTile column
+     */
+    void flipCardUp(int row, int column){
+        MatchingCardsTile card = (MatchingCardsTile) getItem(row, column);
+        card.setFaceUp();
+        if (cardIsUp){
+            flipAllCardsDown();
+        } else {cardIsUp = true;}
+    }
+
+    /**
+     * flips all the cards on the board down
+     */
+    void flipAllCardsDown(){
+        for (int row=0; row < NUM_ROWS; row++){
+            for (int col=0; col < NUM_COLS; col++){
+                MatchingCardsTile card = getCard(row, col);
+                card.setFaceDown();
+            }
+        }
+    }
 }
